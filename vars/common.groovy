baa15611994232dc715057c3a918b17f6ba46221
FinnW24 <161514564+FinnW24@users.noreply.github.com>
2024-03-28 14:19:04 -0500

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,6 +3,7 @@ def call(String stageName){
   if ("${stageName}" == "Build")
      {
        sh "mvn clean package"
+       sh "build complete"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
